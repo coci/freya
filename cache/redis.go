b198f56b94bc8107745f156f3d0d6f7ab2da18c8
soroush <soroush.safari1992Gmailc.om>
2022-06-04 16:40:01 +0430

    add : redis conn

--- cache/redis.go
+++ cache/redis.go
@@ -1,6 +1,9 @@
 package cache
 
 import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"strconv"
 	"time"
 )
 
@@ -8,6 +11,18 @@ type Redis struct {
 	Host string
 	Db   int8
 	Port int16
+}
+
+var ctx = context.Background()
+
+func (r Redis) getRedisConnection() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     r.Host + ":" + strconv.Itoa(int(r.Port)),
+		Password: "",        // no password set
+		DB:       int(r.Db), // use default DB
+	})
+
+	return client
 }
 
 func (r Redis) AddCounter(ip string, duration time.Duration) {
