bdab33503da02ddfab8a820d256db42e3d993a43
soroush <soroush.safari1992Gmailc.om>
2022-06-04 18:01:56 +0430

    add : limiter builder

--- freya.go
+++ freya.go
@@ -1,8 +1,36 @@
 package freya
 
-import "time"
+import (
+	"github.com/coci/freya/cache"
+	"sync"
+	"time"
+)
 
 type Freya struct {
 	requestNumber int16
+	Cache         cache.ICache
 	Duration      time.Duration
+}
+
+var lock = &sync.Mutex{}
+
+// hold single instance of Freya
+var singleFreyaInstance *Freya
+
+// NewLimiter create or return Freya instance ( using singleton )
+func NewLimiter(request int16, duration time.Duration, cacheType string) *Freya {
+	if singleFreyaInstance != nil {
+		return singleFreyaInstance
+	} else {
+
+		lock.Lock()
+		defer lock.Unlock()
+
+		// get proper cache handler with user input
+		cacheType := cache.GetCacheHandler(cacheType)
+
+		singleFreyaInstance := &Freya{requestNumber: request, Cache: cacheType, Duration: duration}
+
+		return singleFreyaInstance
+	}
 }
